15dc73ee67d607b3d1d9dc68aad9ec98e40d258d
HyeonSoo <21port@naver.com>
2019-08-05 17:32:33 +0900

    swexpert|190805

--- 1979.py
+++ 1979.py
@@ -1,48 +1,50 @@
 import sys
-sys.stdin = open('input.txt')
-
-def ispass(y,x):
-    return 0<=y<N and 0<=x<N and not arr[y][x]
-
-dy = [0,1,0,-1]
-dx = [1,0,-1,0]
+sys.stdin = open('1979.txt')
 
 
 T = int(input())
-N, K = map(int, input().split())
 
-arr = []
-for i in range(N):
-    arr.append(list(map(int, input().split())))
+for a in range(T):
+    arr = []
+    N, K = map(int, input().split())
+    for i in range(N):
+        arr.append(list(map(int, input().split())))
 
-print(arr)
-fit = 0
-cnt = 0
+    fit = 0
 
-y,x,d = 0,0,0
-
-for i in range(N**2):
-    if arr[y][x] == 1:
-        cnt += 1
-        ny, nx = y + dy[d], x + dx[d]
-        if cnt == K :
-            print('{} 들어갈 자리 하나'.format(K))
-            if not ispass(ny, nx):
-                print('다음줄')
-                ny, nx = y + 1, 0
-                fit += 1
-                y, x = ny, nx
-            if ispass(y,x) and arr[y][x+1] == 0:
-                print('영역내')
-                fit += 1
-
-    elif arr[y][x] == 0:
+    for j in range(N):
         cnt = 0
-        ny, nx = y + dy[d], x + dx[d]
-        if not ispass(ny, nx):
-            print('다음줄')
-            ny, nx = y + 1, 0
-        y, x = ny, nx
+        for i in range(N):
+            if arr[j][i] == 1:
+                cnt += 1
+                if cnt == K:
+                    if i == N-1:
+                        fit += 1
+                        cnt = 0
+                    elif arr[j][i+1] == 0:
+                        fit += 1
+                        cnt = 0
+                elif cnt > K:
+                    cnt = 0
+            else:
+                cnt = 0
 
+    for i in range(N):
+        cnt = 0
+        for j in range(N):
+            if arr[j][i] == 1:
+                cnt += 1
+                if cnt == K:
+                    if j == N-1:
+                        fit += 1
+                        cnt = 0
+                    elif arr[j+1][i] == 0:
+                        fit += 1
+                        cnt = 0
+                elif cnt > K:
+                    cnt = 0
+            else:
+                cnt = 0
 
-print(fit)
+    print('#{} {}'.format(a+1, fit))
+

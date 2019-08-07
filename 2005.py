a8e05ab13c31c106cd71d0526dc7ad2914ccb0c4
HyeonSoo <21port@naver.com>
2019-08-07 17:04:00 +0900

    190807|D2

--- 2005.py
+++ 2005.py
@@ -1,8 +1,27 @@
 import sys
-sys.stdin = open('2001.txt')
+sys.stdin = open('2005.txt')
 
 
 T = int(input())
+build = [[1]]
+lst = [1, 1]
 
-N = int(input())
+for a in range(1,T+1):
+    N = int(input())
+    if N == 1:
+        print('#{} \n1'.format(a))
+    else:
+        lst2 = [1]
+        for i in range(1,N):
+            lst2.append(lst[i]+lst[i-1])
+        lst2.append(1)
+        build.append(lst)
 
+        lst = lst2
+        print('#{}'.format(a))
+        for y in range(len(build)):
+            for x in range(len(build[y])):
+                if x == len(build[y])-1:
+                    print(build[y][x])
+                else:
+                    print(build[y][x],end=' ')

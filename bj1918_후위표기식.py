04938cbf2af8601a3e4b748f10d14ff57d3c73c7
whitelbue <21port@naver.com>
2019-11-11 20:47:02 +0900

    191111|1918_stack_후위표기법

--- bj1918_후위표기식.py
+++ bj1918_후위표기식.py
@@ -1,37 +1,27 @@
-isp = ['(', '+', '*']
-icp = [0, '+', '*', '(']
+priority = {
+    '*': 2,
+    '/': 2,
+    '+': 1,
+    '-': 1,
+    '(': 0
+}
 
 data = input()
 
-fixlst = []
 stack = []
-cnt = 0
 
-for i in range(len(data)):
-    # print('i:',i,'data[i]:',data[i])
-    # print('stack:',stack)
-    # print('fixlst: ',fixlst)
-    if data[i] == ')':
-      while stack[-1] != '(':
-          fixlst.append(stack.pop())
-          if stack[-1] == '(':
-              stack.pop()
-              break
-
-    elif data[i] in isp :
-        if stack:
-            if icp.index(data[i]) > isp.index(stack[-1]):
-                stack.append(data[i])
-            else:
-                while stack and icp.index(data[i]) <= isp.index(stack[-1]):
-                    fixlst.append(stack.pop())
-                stack.append(data[i])
-        else:
-            stack.append(data[i])
+for i in '('+data+')':
+    if 'A' <= i <= 'Z':
+        print(i,end='')
+    elif i == '(':
+        stack.append(i)
+    elif i == ')':
+        while True:
+            temp = stack.pop()
+            if temp == '(':
+                break
+            print(temp,end='')
     else:
-        fixlst.append(data[i])
-
-while len(stack):
-    fixlst.append(stack.pop())
-
-print(''.join(list(map(str,fixlst))))
+        while stack[-1] != '(' and priority[i] <= priority[stack[-1]]:
+            print(stack.pop(), end='')
+        stack.append(i)

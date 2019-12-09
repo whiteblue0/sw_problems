111fc3130390370f5f455109ac56ec7341a5778f
whiteblue0 <21port@naver.com>
2019-12-09 17:59:00 +0900

    191209|BFS: bj2573_빙산

---
+++ bj2573_빙산.py
@@ -0,0 +1,77 @@
+from collections import deque
+# 우, 하, 좌, 상
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+
+def ispass(y,x):
+    return 0<=y<N and 0<=x<M and data[y][x] and not visited[y][x]
+
+def bfs(sy,sx):
+    global cnt, flag
+    que = deque()
+    visited[sy][sx] = 1
+    que.append((sy,sx))
+    while que:
+        y,x = que.popleft()
+        for i in range(4):
+            ny,nx = y+dy[i],x+dx[i]
+            if ispass(ny,nx):
+                visited[ny][nx] = cnt
+                if cnt > 1:
+                    flag = True
+                    break
+                que.append((ny,nx))
+    cnt += 1
+
+
+
+N, M = map(int, input().split())
+data = [list(map(int, input().split())) for _ in range(N)]
+iceberg = []
+visited = [[0]*M for _ in range(N)]
+
+icemax = 0
+for i in range(N):
+    for j in range(M):
+        if data[i][j]:
+            iceberg.append([i,j,data[i][j]])
+        if data[i][j] > icemax:
+            icemax = data[i][j]
+
+result = 0
+
+for t in range(icemax+1):
+    cnt = 1
+    flag = False
+    for i in range(len(iceberg)):
+        if not visited[iceberg[i][0]][iceberg[j][1]]:
+            bfs(iceberg[i][0],iceberg[j][1])
+        if flag:
+            result =  t
+            break
+
+    if not flag:
+        for i in range(len(iceberg)):
+            ice = iceberg[i][2]
+            water = 0
+            y = iceberg[i][0]
+            x = iceberg[i][1]
+            for j in range(4):
+                ny,nx = y+dy[j],x+dx[j]
+                if data[ny][nx] == 0:
+                    water += 1
+            if ice > water:
+                ice -= water
+            else:
+                ice = 0
+            iceberg[i][2] = ice
+        for i in range(len(iceberg)):
+            y = iceberg[i][0]
+            x = iceberg[i][1]
+            data[y][x] = iceberg[i][2]
+    print(t)
+    for i in range(N):
+        print(data[i])
+    print(result)
+    print()
+# print(result)

abc6ed2e999dd4bc4d22d866e0585fb83b9180ca
HyeonSoo <21port@naver.com>
2019-12-17 17:49:19 +0900

    191217|simulation:nedd roatate process

---
+++ bj17822_원판 돌리기.py
@@ -0,0 +1,88 @@
+from collections import deque
+
+def rotate(lst,dir,k,a):
+    if dir:
+        for i in range(k):
+            lst.append(lst.popleft())
+    else:
+        for i in range(k):
+            lst.appendleft(lst.pop())
+    print(a)
+    return lst
+
+
+N,M,T = map(int, input().split())
+board = [deque() for _ in range(N)]
+for i in range(N):
+    temp = list(map(int, input().split()))
+    for j in temp:
+        board[i].append(j)
+
+# cmd[i] = [x,d,k]
+cmd = [list(map(int, input().split())) for _ in range(T)]
+
+
+cnt = 0
+while cnt < T:
+    X = cmd[cnt][0]
+    D = cmd[cnt][1]
+    K = cmd[cnt][2]
+    print("X,D,K",X,D,K)
+    for i in range(N):
+        if (i+1) % X == 0:
+            temp = rotate(board[i],D,K,i+1)
+            board[i] = temp
+
+
+    checker = [[0]*M for _ in range(N)]
+    flag = 0
+    for i in range(N):
+        for j in range(M):
+            if i < N-1 and board[i][j] == board[i+1][j]:
+                checker[i][j] = 1
+                checker[i+1][j] = 1
+                flag = 1
+            if board[i][j] == board[i][j-1]:
+                checker[i][j] = 1
+                checker[i][j-1] = 1
+
+
+    for i in range(N):
+        for j in range(M):
+            if checker[i][j]:
+                board[i][j] = 0
+
+    for i in range(N):
+        print(board[i])
+    print()
+
+    if not flag:
+        avg = 0
+        mysum = 0
+        mylen = 0
+        for i in range(N):
+            for j in range(M):
+                if board[i][j]:
+                    mylen += 1
+                    mysum += board[i][j]
+        avg = mysum / mylen
+
+        for i in range(N):
+            for j in range(M):
+                if board[i][j] == 0:
+                    continue
+                elif board[i][j] > avg:
+                    board[i][j] -= 1
+                elif board[i][j] < avg:
+                    board[i][j] += 1
+
+    cnt += 1
+
+ans = 0
+for i in range(N):
+    for j in range(M):
+        ans += board[i][j]
+
+# for i in range(N):
+#     print(board[i])
+print(ans)

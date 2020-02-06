1a5e5f5054f930cbc0bd09dc9f8f499311da582d
whitelbue <21port@naver.com>
2020-02-07 00:07:20 +0900

    update ans condition:0 to maxvalue

--- bj13460_구슬 탈출 2.py
+++ bj13460_구슬 탈출 2.py
@@ -18,52 +18,46 @@ def tilt(d,sry,srx,sby,sbx):
         elif [nby, nbx] == [sry,srx]:
             isred = 1
         elif data[nby][nbx] == 9:
-            nby -= dy[d]
-            nbx -= dx[d]
-            by, bx = nby, nbx
             break
         by,bx = nby,nbx
     if isred:
         by -= dy[d]
         bx -= dx[d]
 
-    if not flag:
-        while True:
-            nry,nrx = ry+dy[d],rx+dx[d]
-            if [nry,nrx] == hole and not flag == -1:
-                ry, rx = nry, nrx
-                flag = 1
-                break
-            elif [nry, nrx] == [sby,sbx]:
-                isblue = 1
-            elif data[nry][nrx] == 9:
-                nry -= dy[d]
-                nrx -= dx[d]
-                ry, rx = nry, nrx
-                break
-            ry,rx = nry,nrx
-        if isblue:
-            ry -= dy[d]
-            rx -= dx[d]
+    while True:
+        nry,nrx = ry+dy[d],rx+dx[d]
+        if [nry,nrx] == hole and not flag == -1:
+            ry, rx = nry, nrx
+            flag = 1
+            break
+        elif [nry, nrx] == [sby,sbx]:
+            isblue = 1
+        elif data[nry][nrx] == 9:
+            break
+        ry,rx = nry,nrx
+    if isblue:
+        ry -= dy[d]
+        rx -= dx[d]
 
     return (ry,rx,by,bx,flag)
 
 def dfs(d,ry,rx,by,bx,cnt):
     global ans
     cnt += 1
+    if cnt > 10:
+        return
+
+    if cnt >= ans:
+        return
     (ry,rx,by,bx,flag) = tilt(d,ry,rx,by,bx)
 
-    if flag == -1 and not ans > 0:
-        ans = flag
+    # flag : 0 or 1 or -1
+    # ans: 0 or cnt
+    if flag == -1:
         return
+
     elif flag == 1:
-        if ans == 0 or ans == -1:
-            ans = cnt
-        elif cnt < ans:
-            ans = cnt
-        return
-    elif cnt == 10:
-        return
+        ans = cnt
 
     for i in range(4):
         if i != d and not visited[cnt]:
@@ -91,26 +85,12 @@ for i in range(N):
             data[i][j] = -1
             hole = [i,j]
 
-ans = 0
+ans = 987654321
 visited = [0]*11
 for i in range(4):
     dfs(i,red[0],red[1],blue[0],blue[1],0)
 
-if ans == 0:
+if ans == 987654321:
     ans = -1
 print(ans)
 
-# testCase
-10 10
-##########
-#RB....#.#
-#..#.....#
-#........#
-#.O......#
-#...#....#
-#........#
-#........#
-#.......##
-##########
-
-output: 10
